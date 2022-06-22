d0d212b85d73e93db1abadf283e7fd25963ee440
Sergio Peris <dev.sertxu@gmail.com>
2022-06-22 16:26:00 +0200

    wip

--- src/Preview.ts
+++ src/Preview.ts
@@ -3,11 +3,6 @@ import { marked } from 'marked';
 
 const renderer = new marked.Renderer();
 renderer.link = function (href: string, title: string = '', text: string) {
-  // If the href is an image, we want to add the image to the DOM
-  if (href === text && href.match(/\.(jpeg|jpg|gif|png)$/)) {
-    return `<img src="${href}" alt="${title}" />`;
-  }
-
   return `<a target="_blank" title="${title}" href="${href}">${text || href}` + '</a>';
 }
 
@@ -20,7 +15,6 @@ marked.use({ renderer })
 export default class Preview {
 
   static renderMarkdown(markdown: string): string {
-    // return DOMPurify.sanitize(marked(markdown, { renderer: renderer }));
     return DOMPurify.sanitize(marked.parse(markdown), { ADD_ATTR: ['target'] });
   }
 
